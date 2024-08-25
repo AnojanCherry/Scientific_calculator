6d9fb8a236a7f12b3883673cffa77c3524f2da70
AnojanCherry <anojancherryaji@gmail.com>
2024-08-25 01:05:51 +0100

    bracket, power

--- calculator_cli.py
+++ calculator_cli.py
@@ -1,3 +1,5 @@
+import copy
+
 class calculator_cli:
     def __init__(self):
         """
@@ -23,11 +25,13 @@ class calculator_cli:
         """
 
         while True:
+            # print("Here")
+            print(eqr)
             try:
                 int(eqr)
                 return(eqr)
             except:
-                eqr = self.subract(eqr)
+                eqr = self.brackets(eqr)
     
     def brackets(self, eqr:str)->str:
         """
@@ -39,6 +43,85 @@ class calculator_cli:
         Returns:
             str: returns after substituting string
         """
+
+        if "(" in eqr:
+            # tmp = []
+            count = 0
+            ind_s = 0
+            ind_e = 0
+            for ind, eq_i in enumerate(eqr):
+                if "(" in eq_i:
+                    ind_s = ind
+                    count +=1
+                if (")" in eq_i):
+                    if (ind_e<=ind_s):
+                        ind_e = ind 
+                    count -= 1
+                # print(f"{ind}. {eq_i}")
+            # for i in eqr:
+            #     if "(" in i:
+            #         tmp.append(eq_splt)
+            #         eq_splt = ""
+            #         count += 1
+            #     elif ")" in i:
+            #         tmp.append(eq_splt)
+            #         eq_splt = ""
+            #         count -= 1
+            #     else:
+            #         eq_splt += i
+            # print(tmp)
+            # if tmp[0] == "":
+            #     del tmp[0]
+            # if tmp[-1] == "":
+            #     del tmp[-1]
+            # input(tmp)
+            # input(count)
+            if count <0:
+                raise Exception("More ) than (")
+            elif count>0:
+                raise Exception("More ( than )")
+            # else:
+            #     input(tmp)
+            eqr_s = eqr[:ind_s]
+            eqr_splt = eqr[ind_s+1:ind_e]
+            eqr_e = eqr[ind_e+1:]
+            # print(eqr_s)
+            # print(eqr_splt)
+            # input(eqr_e)
+            # input(f"Calc: {eqr_splt}")
+            eqr_splt = self.calculate(eqr_splt)
+            if eqr_s == "":
+                if eqr_e == "":
+                    return eqr_splt
+                else:
+                    try:
+                        int(eqr_e[0])
+                        return eqr_splt+"*"+eqr_e
+                    except:
+                        return eqr_splt+eqr_e
+            elif eqr_e == "":
+                try:
+                    int(eqr_s[-1])
+                    return eqr_s+"*"+eqr_splt
+                except:
+                    return eqr_s+eqr_splt
+            else:
+                eqr_return = ""
+                try:
+                    int(eqr_s[-1])
+                    eqr_return+=eqr_s+"*"+eqr_splt
+                except:
+                    eqr_return+=eqr_s+eqr_splt
+
+                try:
+                    int(eqr_e[0])
+                    eqr_return+="*"+eqr_e
+                except:
+                    eqr_return+=eqr_e
+                return eqr_return
+        else:
+            # print("Here")
+            return self.indices(eqr)
     
     def indices(self, eqr:str)->str:
         """
@@ -50,6 +133,45 @@ class calculator_cli:
         Returns:
             str: returns after substituting string
         """
+        if "**" in eqr:
+            indice_char = ["**"]
+            ind = eqr.index("**")
+            i=1
+            try:
+                while ind-i>=0:
+                    # input(eqr[ind-i])
+                    int(eqr[ind-i])
+                    i+=1
+                ind_s=0
+                eqr_s=""
+            except:
+                ind_s = ind-i+1
+                eqr_s = eqr[:ind_s]
+            print(eqr_s)
+            i=ind+2
+            try:
+                while i<=len(eqr):
+                    # input(eqr[i])
+                    int(eqr[i])
+                    i+=1
+                ind_e=-1
+                eqr_e=""
+            except:
+                ind_e = i
+            eqr_e = eqr[ind_e:]
+            # input(f"{eqr_s}\t{eqr_e}")
+            # input(f"{eqr[ind_s:ind_e]}")
+            eqr_splt = eqr[ind_s:ind_e]
+            base,powe = eqr_splt.split("**")
+            # input(f"{base}\t{powe}")
+            eqr_splt = pow(int(base), int(powe))
+            return eqr_s+str(eqr_splt)+eqr_e
+            # if ind_s==0:
+            #     if ind_e==0:
+            #         print(eqr)
+            # input()
+        else:
+            return self.division(eqr)
     
     def division(self, eqr:str)->str:
         """
@@ -83,9 +205,6 @@ class calculator_cli:
         Returns:
             str: returns after substituting string
         """
-
-    def subract_verify(self, eqr:str):
-        return
     
     def subract(self, eqr:str)->str:
         """
--- runMe_cli.py
+++ runMe_cli.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 from calculator_cli import calculator_cli as cli
 
 if __name__ == "__main__":
@@ -8,20 +9,28 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser(description="Take cli inputs")
-    parser.add_argument("--equation", type=str, default="None", help="Pass in an equation to get result.")
+    parser.add_argument("--equation", "-eq", type=str, default="None", help="Pass in an equation to get result.")
     args = parser.parse_args()
 
     ## Unparse
     # unparse equations
     if(args.equation != "None"):
-        for i in range(ord("a"),ord("z")+1):
-            if chr(i) in args.equation:
-                error_msg = f"Can't solve for \"{chr(i)}\""
-                raise Exception(error_msg)
-        for i in range(ord("A"),ord("Z")+1):
-            if chr(i) in args.equation:
-                error_msg = f"Can't solve for \"{chr(i)}\""
-                raise Exception(error_msg)
-        cal.calculate(args.equation)
+        tbdl = copy.copy(args.equation)
+        for i in range(10):
+            tbdl=tbdl.replace(str(i),"")
+        tbdl=tbdl.replace("*","")
+        tbdl=tbdl.replace("(","")
+        tbdl=tbdl.replace(")","")
+        tbdl=tbdl.replace("/","")
+        tbdl=tbdl.replace("+","")
+        tbdl=tbdl.replace("-","")
+        print(len(tbdl))
+        # if chr(i) in args.equation:
+        #         error_msg = f"Can't solve for \"{chr(i)}\""
+        #         raise Exception(error_msg)
+        if (len(tbdl))>0:
+            error_msg = f"Can't solve for \"{args.equation}\", unknown characters: {tbdl}"
+            raise Exception(error_msg)
+        print(f"Calculated value: {cal.calculate(args.equation)}")
     else:
         print("No equations to process")
